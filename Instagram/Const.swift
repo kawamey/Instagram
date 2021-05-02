4de0056196991bd9b99d53bd7ba2e488f82be7a7
kawamey <chocolattecandy1846@icloud.com>
2021-05-02 17:41:43 +0900

    Const.swift追加

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 川目悠貴 on 2021/04/20.
+//
+
+import UIKit
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
